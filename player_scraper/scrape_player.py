d8fd08fed33c60fb62c85625fbef9aef32600fd9
mctyn64 <bneuman@unknown949426010a1e.att.net>
2014-04-18 20:25:03 -0500

    clean up

--- player_scraper/scrape_player.py
+++
@@ -1,7 +0,0 @@
-from br_player_finder import get_player_soup
-from br_player_scraper import make_player_stats
-
-def scrape_player(player_name):
-    soup = get_player_soup(player_name)
-    stats = make_player_stats(soup)
-    return stats
