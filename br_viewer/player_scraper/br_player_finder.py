f15e7a0271db70c99ecac624af65b8a081f0b8c7
mctyn64 <bneuman@unknown949426010a1e.att.net>
2014-04-19 17:12:03 -0500

    Rewrote get_player_url method to call database. Reorganized files because I dont know Python modules/paths well enough yet.

--- br_viewer/player_scraper/br_player_finder.py
+++ br_viewer/player_scraper/br_player_finder.py
@@ -1,5 +1,7 @@
 from soupify import soupify, get_url_content
+from db_setup import Player, Session
 
+session = Session()
 
 def get_player_soup(player_name):
     url = get_player_url(player_name)
@@ -20,15 +22,34 @@ def get_index_url_from_name(player_name):
     return "http://basketball-reference.com/players/" + letter
 
 def get_player_url(player_name):
+    url = get_player_url_from_db(player_name)
+
+    if not url:
+        url = get_player_url_from_br(player_name)
+    
+        if not url:
+            return None
+
+    absolute_url = "http://basketball-reference.com" + url
+    return absolute_url
+
+def get_player_url_from_br(player_name):
     index_url = get_index_url_from_name(player_name)
     souped_index = soupify(index_url)
     link_finder = player_link_finder(player_name)
-    player_link = souped_index.find(link_finder)
-    relative_link = player_link['href']
-    absolute_url = "http://basketball-reference.com" + relative_link
-    return absolute_url
+    player_link_element = souped_index.find(link_finder)
+    if player_link_element:
+        link = player_link_element['href']
+        return link
+    else:
+        return None
 
-
+def get_player_url_from_db(player_name):
+    player = session.query(Player).filter(Player.name.like(player_name.lower())).first()
+    if player:
+        return player.url
+    else:
+        return None
 
 def player_link_finder(name):
     def link_finder(element):
