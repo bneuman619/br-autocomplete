256ad01915c5734a35f575a550b411dfed02b3c7
mctyn64 <bneuman@unknown949426010a1e.att.net>
2014-04-26 18:59:42 -0500

    Still fixing virtualenv

--- extensions/extension_399.py
+++
@@ -1,6 +0,0 @@
-from player_data import get_player_data
-
-def load_ipython_extension(ipython):
-    ipython.enable_matplotlib()
-    player = get_player_data("ron harper")
-    ipython.push('player')
